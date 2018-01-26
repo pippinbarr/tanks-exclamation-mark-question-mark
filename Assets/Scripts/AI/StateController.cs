87e83b7042c0e1498ec5ac83ab8625145af182a4
Pippin Barr <pippin.barr@gmail.com>
2018-01-26 13:50:09 -0500

    Created base Unity 2017 project with Pluggable AI and minor modifications of my own, README.md, .gitignore, deleted PostProcessing effects, eliminated Realtime GI
    ? This commit represents the terrible moment at which I decided to just abandon the original repository for this game due to total confusion at both the technical and conceptual layers. The hope and dream is that by starting fresh I can learn from my past mistakes (indeed they're still there in the other repo) and hit the project running with fresh(er) eyes and soul.

---
+++ Assets/Scripts/AI/StateController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Complete;
+
+public class StateController : MonoBehaviour {
+
+
+    public TankState currentState;
+	public EnemyStats enemyStats;
+	public Transform eyes;
+    public TankState remainState;
+
+
+	[HideInInspector] public NavMeshAgent navMeshAgent;
+	[HideInInspector] public Complete.TankShooting tankShooting;
+	[HideInInspector] public List<Transform> wayPointList;
+    [HideInInspector] public int nextWayPoint;
+    [HideInInspector] public Transform chaseTarget;
+    [HideInInspector] public float stateTimeElapsed = 0f;
+
+	private bool aiActive;
+
+
+	void Awake () 
+	{
+		tankShooting = GetComponent<Complete.TankShooting> ();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+	public void SetupAI(bool aiActivationFromTankManager, List<Transform> wayPointsFromTankManager)
+	{
+		wayPointList = wayPointsFromTankManager;
+		aiActive = aiActivationFromTankManager;
+		if (aiActive) 
+		{
+			navMeshAgent.enabled = true;
+		} else 
+		{
+			navMeshAgent.enabled = false;
+		}
+	}
+
+    void Update() 
+    {
+        if (!aiActive) return;
+
+        currentState.UpdateState(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (currentState != null && eyes != null)
+        {
+            Gizmos.color = currentState.sceneGizmoColor;
+            Gizmos.DrawWireSphere(eyes.position,enemyStats.lookSphereCastRadius);
+        }
+    }
+
+    public void TransitionToState(TankState nextState) 
+    {
+        if (nextState != remainState) {
+            currentState = nextState;
+            OnExitState();
+        }
+    }
+
+    public bool CheckIfCountdownElapsed(float duration)
+    {
+        stateTimeElapsed += Time.deltaTime;
+        return (stateTimeElapsed >= duration);
+    }
+
+    private void OnExitState() 
+    {
+        stateTimeElapsed = 0f;
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+## The Game
+
+_TANKS!ES_
+
+## Information
+
+__This is the second repository for the TANKS!ES project, the former repository is here https://github.com/pippinbarr/tankses/ for posterity and despair__
+
+* [Code repository](https://github.com/pippinbarr/tankses-ii/) (you're looking at it)
+* [Process documentation](https://github.com/pippinbarr/tankses-ii/wiki)
+* [Blog posts about *TANKS!ES*](http://www.pippinbarr.com/search.html?q=tanks!es)
+* Press Kit coming later!
+
+## License
+
+Christoph Peters's original _Tanks!_ tutorial code and assets are licensed under a [Creative Commons Public Domain Dedication (CC0 1.0)](https://creativecommons.org/publicdomain/zero/1.0/)
+
+To the best of my knowledge, the starter scripts and other tutorial material in Unity's [Live Session: Pluggable AI With Scriptable Objects](https://unity3d.com/learn/tutorials/topics/navigation/intro-and-session-goals?playlist=17105) tutorial are also public domain.
+
+Pippin Barr's modifications to _TANKS!ES_ are licensed under a [Creative Commons Attribution-NonCommercial 3.0 Unported License](http://creativecommons.org/licenses/by-nc/3.0/).
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/3.0/88x31.png" /></a>
